6cdce4815c01e3271d7bba7806bb37f1f6c9da23
Nicholas Anderson <nanderson@nanderson.me>
2016-04-01 13:41:18 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const request = require('request');
+const cheerio = require('cheerio');
+const async = require('async');
+const fs = require('fs');
+
+var subreddits = [
+//  "joinrobin",
+  "joinrhino",
+  "joinsquirrel",
+  "joinweasel"
+];
+
+function checkSubreddit(name) {
+  request("https://reddit.com/r/"+name, (err, res, body) => {
+    let $ = cheerio.load(body);
+    let msg = $("code").text().trim();
+    fs.writeFileSync(name+".txt", msg+"\n");
+  });
+}
+
+async.map(subreddits, checkSubreddit);
